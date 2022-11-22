be162a8c92ea53a441ca6aed7ec98e7b6ab173be
Muhammad Umar <muhammadumer091@gmail.com>
2022-11-22 02:54:44 +0000

    bulletprojectile

--- Source/Aiming/Bullets.cpp
+++ Source/Aiming/Bullets.cpp
@@ -3,11 +3,31 @@
 
 #include "Bullets.h"
 
+#include "Components/SphereComponent.h"
+#include "GameFramework/ProjectileMovementComponent.h"
+
+#include "AimEnemy.h"
+
 // Sets default values
 ABullets::ABullets()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	CollisionSphere = CreateDefaultSubobject<USphereComponent>(TEXT("Sphere Collision"));
+	CollisionSphere->InitSphereRadius(20.0f);
+
+	RootComponent = CollisionSphere;
+
+	ProjectileMovement = CreateDefaultSubobject<UProjectileMovementComponent>(TEXT("Projectile Movement"));
+	ProjectileMovement->UpdatedComponent = CollisionSphere;
+
+	ProjectileMovement->InitialSpeed = 3000.03;
+	ProjectileMovement->MaxSpeed = 3000.0f;
+	ProjectileMovement->bRotationFollowsVelocity = true;
+	ProjectileMovement->bShouldBounce = true;
+
+	InitialLifeSpan = 3.0f;
 
 }
 
@@ -16,6 +36,7 @@ void ABullets::BeginPlay()
 {
 	Super::BeginPlay();
 	
+	CollisionSphere->OnComponentBeginOverlap.AddDynamic(this, &ABullets::OnHit);
 }
 
 // Called every frame
